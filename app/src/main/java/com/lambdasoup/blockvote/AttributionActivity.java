0a7b5b96a45cb338ce9bfe76cc50800c4603e76f
Max <docoptix@amsf.de>
2017-02-07 15:03:59 +0100

    app: add attribution screen
    closes #12

---
+++ app/src/main/java/com/lambdasoup/blockvote/AttributionActivity.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2017 mh@lambdasoup.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lambdasoup.blockvote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class AttributionActivity extends AppCompatActivity {
+
+	private final RecyclerView.Adapter<AttributionViewHolder> adapter = new RecyclerView.Adapter<AttributionViewHolder>() {
+
+		@Override
+		public AttributionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_attribution, parent, false);
+			return new AttributionViewHolder(view);
+		}
+
+		@Override
+		public void onBindViewHolder(AttributionViewHolder holder, int position) {
+			String title, text;
+
+			Attribution attribution = Attribution.values()[position];
+			switch (attribution) {
+				case BLOCKVOTE:
+					title = getString(R.string.license_title_blockvote);
+					text = getString(R.string.license_text_blockvote);
+					break;
+
+				case SUPPORTLIB:
+					title = getString(R.string.license_title_supportlib);
+					text = getString(R.string.license_text_supportlib);
+					break;
+
+				case GMS:
+					title = getString(R.string.license_title_playservices);
+					text = GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(getApplicationContext());
+					break;
+
+				default:
+					throw new IllegalArgumentException("unknown attribution key: " + attribution.name());
+			}
+
+			holder.title.setText(title);
+			holder.text.setText(text);
+		}
+
+		@Override
+		public int getItemCount() {
+			return Attribution.values().length;
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_attribution);
+
+		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+		recyclerView.setAdapter(adapter);
+	}
+
+	private enum Attribution {
+		BLOCKVOTE,
+		SUPPORTLIB,
+		GMS
+	}
+
+	private static class AttributionViewHolder extends RecyclerView.ViewHolder {
+
+		private final TextView title;
+		private final TextView text;
+
+		AttributionViewHolder(View itemView) {
+			super(itemView);
+
+			title = (TextView) itemView.findViewById(R.id.title);
+			text = (TextView) itemView.findViewById(R.id.text);
+		}
+	}
+}
--- app/src/main/java/com/lambdasoup/blockvote/MainActivity.java
+++ app/src/main/java/com/lambdasoup/blockvote/MainActivity.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2017 mh@lambdasoup.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.lambdasoup.blockvote;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.UiThread;
@@ -7,6 +24,8 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -68,5 +87,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
 		statsView.swapCursor(null);
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_attributions:
+				startActivity(new Intent(this, AttributionActivity.class));
+				return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
